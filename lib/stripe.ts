334ba6c7cdf4ef0fc86f594b139724c23111da51
Daddyoba12 <daddyoba12@email.com>
2026-03-30 18:00:39 +0100

    fix stripe api version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2026-01-28.clover' as const,
+  apiVersion: '2026-02-25.clover' as const,
 });
 
 // Calculate fees
